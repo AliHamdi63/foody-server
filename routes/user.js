3506687de492eadf7bf63237948d3013d3e2798d
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2022-07-30 09:48:54 +0200

    update

--- routes/user.js
+++ routes/user.js
@@ -1,43 +1,75 @@
-import express, { Router } from "express";
+import express from "express";
 import UserModel from '../models/user.js';
 
 import { verifyTokenAndAuthorizationAsAdmin, verifyTokenAndAuthorization } from "../middleware/verifyToken.js"
 
 let router = express.Router()
 
-router.get("/admin", async (req, res) => {
+// get all admins
+router.get("/admin",verifyTokenAndAuthorizationAsAdmin, async (req, res) => {
     console.log("--------------------------------------------------");
-    const alladmins = await UserModel.find({ isAdmin: true })
-    res.send(alladmins)
+    try{
+        const alladmins = await UserModel.find({ isAdmin: true })
+        res.status(200).json(alladmins)
+
+    }catch(err){
+        res.status(400).json(err)
+    }
 })
+
+//get all users
 router.get("/", verifyTokenAndAuthorizationAsAdmin, async (req, res) => {
-    const allusers = await UserModel.find({})
-    res.send(allusers)
+    try{
+        const allusers = await UserModel.find({})
+        res.status(200).json(allusers)
+
+    }catch(err){
+        res.status(400).json(err)
+    }
 })
+
+//get user by ID
 router.get("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
-    const user = await UserModel.findById({ _id: req.params.id })
-    res.send(user)
-})
-
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-    let updateeduser = await UserModel.findOne({ _id: req.params.id })
-    console.log(req.body, "body", updateeduser);
-    let keys = Object.keys(req.body)
-    keys.forEach((key => {
-        updateeduser[key] = req.body[key]
-    }))
-    await updateeduser.save()
-    console.log(keys, "------", updateeduser);
-    res.send(updateeduser)
-})
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-    const deleteduser = await UserModel.findByIdAndDelete(req.params.id)
-    if (deleteduser) {
-        res.status(200).send(deleteduser.email + " deleted successfully")
-
+    try {
+        const user = await UserModel.findById(req.params.id)
+        res.status(200).json(user)
+        
+    } catch (err) {
+        res.status(400).json(err)
     }
-    res.status(500).send("error deleting user")
+})
+
+//edit user
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+
+    try {
+        let updateeduser = await UserModel.findOne({ _id: req.params.id })
+
+        let keys = Object.keys(req.body)
+        keys.forEach((key => {
+            updateeduser[key] = req.body[key]
+        }))
+        await updateeduser.save()
+
+        res.status(200).json(updateeduser)
+        
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+//delete user
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+
+        try {
+            const deleteduser = await UserModel.findByIdAndDelete(req.params.id)
+            res.status(200).json(deleteduser)
+            
+        } catch (err) {
+            res.status(400).json(err)
+        }
+
 
 })
 
