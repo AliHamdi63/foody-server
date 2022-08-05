1ff9bdc785d407bcc1a51f3272163c3786c9bf34
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2022-08-05 16:17:41 +0200

    update

--- routes/order.js
+++ routes/order.js
@@ -206,7 +206,7 @@ router.get("/monthly/spending",verifyTokenAndAuthorizationAsAdmin, async (req, r
           },
         },
       ]);
-      res.status(200).json(income);
+      res.status(200).json(income[0]['total']);
     } catch (err) {
       res.status(500).json(err);
     }
