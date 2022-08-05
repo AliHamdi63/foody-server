ff5e1794b028142244620fdc36b1c3c740f5171b
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2022-08-05 20:24:58 +0200

    update

--- routes/order.js
+++ routes/order.js
@@ -125,7 +125,7 @@ router.get("/monthly/income",verifyTokenAndAuthorizationAsAdmin, async (req, res
  
     try {
       const income = await OrderModel.aggregate([
-        { $match: { createdAt: { $gte: lastYear } } },
+        { $match: { $and:[{createdAt: { $gte: lastYear }},{user:req.user.id}] } },
         {
           $project: {
             month: { $month: "$createdAt" },
